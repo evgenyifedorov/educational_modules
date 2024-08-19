905271306c4bfa43fb6ad3358a598ab3326cb740
evgen <evgen1973108@gmail.com>
2024-08-19 21:25:32 +0300

    add educmoduls_1.2

--- users/views.py
+++ users/views.py
@@ -12,3 +12,11 @@ class UserViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        """
+        Хэширует создаваемый при регистрации пароль.
+        """
+        instance = serializer.save(is_active=True)
+        instance.set_password(instance.password)
+        instance.save()
